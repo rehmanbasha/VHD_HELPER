39e0907358bd84e0dc4cf4641dd121ad8942f657
Rehman Basha <rehmanbasha01@gmail.com>
2015-10-02 19:07:23 +0530

    Initial checkin of VHD helper tool.

---
+++ VHD_HELPER/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices; 
+
+
+namespace VHD_HELPER
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            status_text.Text = "Ready";           
+        }
+        List <MyDataGridColumns> data = new List<MyDataGridColumns>();
+        BrushConverter bc = new BrushConverter();
+        private void AttachButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            //var folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    //var obj = new MyDataGridColumns();                    
+                    var file = fileDialog.FileName;
+                    
+                    //filename_lbl.Content = "Selected File:" +file.ToString();
+                    bool attached=mount.OpenAndAttachVHD(file.ToString());
+                    if (attached)
+                    {                       
+                        statusbar.Background = (Brush)bc.ConvertFrom("#2E8DEF"); 
+                        status_text.Text = "Selected File: " + file.ToString();
+                        data.Add(new MyDataGridColumns()
+                        {
+                            Filename = file.ToString(),
+                            Disksignature = "1234"
+                        });
+                        VHDDataGrid.ItemsSource = data;
+                        VHDDataGrid.Items.Refresh();
+                    }
+
+                    else
+                    {
+                        statusbar.Background = (Brush)bc.ConvertFrom("#DC572E"); 
+                        status_text.Text = "Attaching VHD Failed ";
+                    }                    
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    break;
+            }
+        }
+
+        private void DetachButton_Click(object sender, RoutedEventArgs e)
+        {
+            int items_count = VHDDataGrid.Items.Count;
+            for (int i = 0; i <items_count ; i++ )
+            {
+                CheckBox chkbx = VHDDataGrid.Columns[0].GetCellContent(VHDDataGrid.Items[i]) as CheckBox;
+             
+                if (chkbx.IsChecked == true)
+                {
+                    TextBlock vhdname_column = VHDDataGrid.Columns[1].GetCellContent(VHDDataGrid.Items[i]) as TextBlock;
+                    string vhd = vhdname_column.Text;
+                    bool detached = mount.OpenAndDetachVHD(vhd);
+                    if (detached)
+                    {
+                        BrushConverter bc = new BrushConverter();
+                        statusbar.Background = (Brush)bc.ConvertFrom("#2E8DEF");
+                        status_text.Text = "Detached File: " + vhd;
+                        //data.RemoveAt(i);                        
+                    }
+                    else
+                    {
+                        statusbar.Background = (Brush)bc.ConvertFrom("#DC572E");
+                        status_text.Text = "Detaching VHD Failed " + vhd;
+                    }                    
+                }
+            }
+            // Update Data Grid Table
+            VHDDataGrid.ItemsSource = data;
+            VHDDataGrid.Items.Refresh();
+        }
+    }
+
+    public class MyDataGridColumns
+    {
+       public string Filename { get; set; }
+       public string Disksignature { get; set; }
+    }
+}
---
+++ VHD_HELPER/vhdlib.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; 
+
+namespace VHD_HELPER
+{
+    class vhdlib
+    {
+
+       
+    }
+
+    public class mount
+    {
+        [DllImport("DiskLibrary.dll", SetLastError = true)]
+        public static extern int GetVolumeName(int disknumber, int partitionnumber, [In, Out] StringBuilder VolumeName);
+
+        [DllImport("DiskLibrary.dll", SetLastError = true)]
+        public static extern int GetDiskNumber([In, Out] StringBuilder VolumeName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetVolumePathNamesForVolumeNameW([MarshalAs(UnmanagedType.LPWStr)] string lpszVolumeName,
+           [MarshalAs(UnmanagedType.LPWStr)] string lpszVolumePathNames, uint cchBuferLength, ref UInt32 lpcchReturnLength);
+
+        [DllImport("DiskLibrary.dll", SetLastError = true)]
+        public static extern bool OpenAndAttachVHD([MarshalAs(UnmanagedType.LPWStr)] string vhdfile);
+
+        [DllImport("DiskLibrary.dll", SetLastError = true)]
+        public static extern bool OpenAndDetachVHD([MarshalAs(UnmanagedType.LPWStr)] string vhdfile);
+
+        [DllImport("DiskLibrary.dll", SetLastError = true)]
+        public static extern bool OpenAndGetPhysVHD([MarshalAs(UnmanagedType.LPWStr)] string vhdfile, [In, Out] StringBuilder physicaldrive);
+
+    }
+}
